5e6aa3044ad72a326583be173df270df5ab59ac8
Janani-A-SBITO3 <104630211+Janani-A-SBITO3@users.noreply.github.com>
2022-04-30 07:53:41 +0530

    Association and static example

--- MainClass.java
+++ MainClass.java
@@ -4,20 +4,27 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		C objC = new C();
-		
+
+		//Association example - C object uses keywords to produce a program
 		Program p = objC.writeProgram("C Object");
+               
+                // Association example - C object uses a program to produce an output
 		Output o = objC.executeProgram(p);
 		
 		Java objJava= new Java();
 		
+                //Association example - Java object uses keywords to produce a program
 		Program p1 = objJava.writeProgram("Java Object");
-		Output o1 = objJava.executeProgram(p);
 		
+                //Association example - Java object uses keywords to produce a program
+                Output o1 = objJava.executeProgram(p);
+		
+                
 		ProgrammingLanguage pl1 = new ProgrammingLanguage();
-		pl1.updatePatch();
+		pl1.updatePatch();. //Pl1 -static version is 1
 		
 		ProgrammingLanguage pl2 = new ProgrammingLanguage();
-		pl2.updatePatch();
+		pl2.updatePatch(); //pl2 - version will be 2 independent of object pl1
 		
 	}
 
