47b9bd1a282504fa8ced649890962bea4c260486
Janani-A-SBITO3 <104630211+Janani-A-SBITO3@users.noreply.github.com>
2022-05-02 17:54:49 +0530

    Add files via upload

--- AbstractTest.java
+++ AbstractTest.java
@@ -23,8 +23,22 @@ public class AbstractTest {
 				abstract class MusicalInstrument extends Instrument {
 					abstract void play();
 				}
+				abstract class MedicalInstrument extends Instrument {
+					abstract void diagnose();
+				}
 				abstract class StringBasedMusicalInstrument extends MusicalInstrument {
 					abstract void tuneStrings();
+				}
+				abstract class AirBasedMusicalInstrument extends MusicalInstrument {
+					abstract void blowAir();
+				}
+				abstract class SurgicalMedicalInstrument extends MedicalInstrument{
+					abstract void sterilize();
+				}
+				abstract class PathologicalMedicalInstrument extends MedicalInstrument{
+					abstract void read();
+					abstract void electricitySupply();
+					abstract void scan();
 				}
 				class Guitar extends StringBasedMusicalInstrument {
 
@@ -102,6 +116,122 @@ public class AbstractTest {
 					}
 					
 				}
+				
+				class Flute extends AirBasedMusicalInstrument{
+
+					@Override
+					void blowAir() {
+						System.out.println("Playing the Flute....");
+					}
+
+					@Override
+					void play() {
+						System.out.println("Playing the Flute....");
+					}
+
+					@Override
+					void utilize() {
+						System.out.println("Utilizing the Flute....");
+					}
+				}
+				
+				class SaxoPhone extends AirBasedMusicalInstrument {
+
+					@Override
+					void blowAir() {
+						System.out.println("SaxoPhone is blowing air....");
+						
+					}
+
+					@Override
+					void play() {
+						System.out.println("Saxophone is playing");
+					}
+
+					@Override
+					void utilize() {
+						System.out.println("Saxophone is utilized....");
+					}
+					
+				}
+				
+				class BagPipe extends AirBasedMusicalInstrument{
+
+					@Override
+					void blowAir() {
+						System.out.println("BagPipe is blowing ar in and out....");
+					}
+
+					@Override
+					void play() {
+						System.out.println("BagPipe is playing.....");
+						
+					}
+
+					@Override
+					void utilize() {
+						System.out.println();
+						
+					}
+					
+				}
+				
+				class Needle extends SurgicalMedicalInstrument{
+
+					@Override
+					void sterilize() {
+						System.out.println("Strilizing the needle....");
+						
+					}
+
+					@Override
+					void diagnose() {
+						System.out.println("Diagnozing using the needle...");
+						
+					}
+
+					@Override
+					void utilize() {
+						System.out.println("Utilizing the needle.....");
+						
+					}
+					
+				}
+				class Glucometer extends PathologicalMedicalInstrument{
+
+					@Override
+					void read() {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					void electricitySupply() {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					void scan() {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					void diagnose() {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					void utilize() {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				}
+				
+				
 				
 
 			
